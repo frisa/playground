2a6dcc254ce68d9bbe99c15ad21e3abba4a4cf16
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2024-08-07 14:52:18 +0200

    Update

--- program.cpp
+++ program.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <tuple>
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+
+    std::tuple<int, float, double> t(1, 2.0, 3.0);
+    std::cout << std::get<0>(t) << std::endl;
+    std::cout << std::get<1>(t) << std::endl;
+
     return 0;
 }
